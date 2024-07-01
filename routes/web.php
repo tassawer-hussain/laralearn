d80d71f82e0a6db80b35fda842ea34b3b768cba2
Tassawer Hussain <t.tassawer@gmail.com>
2024-07-01 18:57:22 +0500

    routes/web.php file used to setup routes in application

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Now we can access the home page using http://127.0.0.1:8000/home
+Route::get('/home', function () {
     return view('welcome');
 });
