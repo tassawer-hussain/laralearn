c2b2738cd7c32ff9e512163b2e32d1730d0bd469
Tassawer Hussain <t.tassawer@gmail.com>
2024-07-01 20:01:46 +0500

    Locate and Update the view file

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
+// Views stored inside the resources/views/ folder
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Now we can access the home page using http://127.0.0.1:8000/home
+// Route::get('/home', function () {
 //     return view('welcome');
 // });
 
-// Now we can access the home page using http://127.0.0.1:8000/home
-Route::get('/home', function () {
-    return view('welcome');
-});
+// We can return simple HTML also.
+// Route::get('/home', function () {
+//     return "<h1>Welcome<h1>";
+// });
