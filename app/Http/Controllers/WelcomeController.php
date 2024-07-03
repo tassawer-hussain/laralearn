372929d697a7273b786796a9691367f930849c22
Tassawer Hussain <t.tassawer@gmail.com>
2024-07-03 15:36:29 +0500

    Fetch data from database using Query Builder

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,10 +13,12 @@ class WelcomeController extends Controller
         // 3 different ways to fetch data from the database in Laravel
 
         // 1. Using raw SQL queries
-        $users = DB::select('select * from users');
-        dd($users);
+        // $users = DB::select('select * from users');
+        // dd($users);
         
         // 2. Query Builder
+        $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        dd($users);
         
 
         // 3. Eloquent ORM
